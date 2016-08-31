ab6c18ec3c7ba627bc831a6e2dbc154174ddd360
sch1sch <rof@crosscan.com>
2016-08-31 14:30:28 +0200

    Fix the loader to work with 0.15.7
    Works here. May fail in other cases.

--- src/file-manager.js
+++ src/file-manager.js
@@ -1,43 +1,84 @@
+"use strict";
+
+var _get = require("babel-runtime/helpers/get")["default"];
+
+var _inherits = require("babel-runtime/helpers/inherits")["default"];
+
+var _createClass = require("babel-runtime/helpers/create-class")["default"];
+
+var _classCallCheck = require("babel-runtime/helpers/class-call-check")["default"];
+
 var path = require("path");
 var jspm = require("jspm");
+var jspm_config = require('jspm/lib/config');
+jspm_config.loadSync();
 
-function resolveURL(filename) {
-  return filename.replace(/^file:\/\//, "").replace(/^\/([a-zA-Z]:)\//, "$1/").replace(/^([a-zA-Z])\//, "$1:/").replace(/\//g, path.sep);
-}
+exports.factory = function (less) {
+  return (function (_less$FileManager) {
+    _inherits(_class, _less$FileManager);
 
-exports.factory = function(less) {
-  return class extends less.FileManager {
-    constructor(options) {
-      super();
+    function _class(options) {
+      _classCallCheck(this, _class);
+
+      _get(Object.getPrototypeOf(_class.prototype), "constructor", this).call(this);
       this.options = options;
-      this.loader = new jspm.Loader();
-      this.loader.config({
-        defaultJSExtensions: false
-      });
     }
-    supports(filename) {
-      return filename.startsWith(this.options.prefix);
-    }
-    supportsSync(filename) {
-      return this.supports(filename);
-    }
-    resolve(filename, currentDirectory) {
-      return this.loader.normalize(filename.slice(this.options.prefix.length)).then((filename) => (
-        path.relative(currentDirectory, resolveURL(filename.replace(/\.js$/, "")))
-      ));
-    }
-    resolveSync(filename, currentDirectory) {
-      filename = this.loader.normalizeSync(filename.slice(this.options.prefix.length));
-      return path.relative(currentDirectory, resolveURL(filename.replace(/\.js$/, "")));
-    }
-    loadFile(filename, currentDirectory, ...args) {
-      return this.resolve(filename, currentDirectory).then((filename) => (
-        super.loadFile(filename, currentDirectory, ...args)
-      ));
-    }
-    loadFileSync(filename, currentDirectory, ...args) {
-      filename = this.resolveSync(filename, currentDirectory);
-      return super.loadFileSync(filename, currentDirectory, ...args);
-    }
-  };
+
+    _createClass(_class, [{
+      key: "supports",
+      value: function supports(filename) {
+        return filename.startsWith(this.options.prefix);
+      }
+    }, {
+      key: "supportsSync",
+      value: function supportsSync(filename) {
+        return this.supports(filename);
+      }
+    }, {
+      key: "resolve",
+      value: function resolve(filename, currentDirectory) {
+        return jspm.locate(filename.slice(this.options.prefix.length)).then(function (filename) {
+          var filepath = path.resolve(filename).replace(/\.js$/, '');
+          return path.relative(currentDirectory, filepath);
+        });
+      }
+    }, {
+      key: "resolveSync",
+      value: function resolveSync(filename, currentDirectory) {
+        filename = jspm.locateSync(filename.slice(this.options.prefix.length));
+        filename = path.resolve(filename).replace(/\.js$/, '');
+        return path.relative(currentDirectory, filename);
+      }
+    }, {
+      key: "loadFile",
+      value: function loadFile(filename, currentDirectory) {
+        for (var _len = arguments.length, args = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+          args[_key - 2] = arguments[_key];
+        }
+
+        var _this = this;
+
+        return this.resolve(filename, currentDirectory).then(function (filename) {
+          var _get2;
+
+          return (_get2 = _get(Object.getPrototypeOf(_class.prototype), "loadFile", _this)).call.apply(_get2, [_this, filename, currentDirectory].concat(args));
+        });
+      }
+    }, {
+      key: "loadFileSync",
+      value: function loadFileSync(filename, currentDirectory) {
+        var _get3;
+
+        filename = this.resolveSync(filename, currentDirectory);
+
+        for (var _len2 = arguments.length, args = Array(_len2 > 2 ? _len2 - 2 : 0), _key2 = 2; _key2 < _len2; _key2++) {
+          args[_key2 - 2] = arguments[_key2];
+        }
+
+        return (_get3 = _get(Object.getPrototypeOf(_class.prototype), "loadFileSync", this)).call.apply(_get3, [this, filename, currentDirectory].concat(args));
+      }
+    }]);
+
+    return _class;
+  })(less.FileManager);
 };
